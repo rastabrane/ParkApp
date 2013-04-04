d219e91344f523f7d87bcbdbe1f56c0464bf4519
Buck O'Leary <howardeoleary@gmail.com>
2013-04-04 18:30:29 -0400

    attempting to solve the problem by setting default attributes in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,8 @@ class User < ActiveRecord::Base
       user.name = auth.info.name
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.email = "email@email.com"
+      user.password = nil 
       user.save!
     end
   end
