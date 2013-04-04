ab83822c0b2c846e140debc0f45807a70836c81a
Buck O'Leary <howardeoleary@gmail.com>
2013-04-04 18:21:31 -0400

    preparing to do another migration

--- db/migrate/20130404215937_add_columns_to_users.rb
+++ db/migrate/20130404215937_add_columns_to_users.rb
@@ -1,10 +1,10 @@
 class AddColumnsToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :provider, :string,
-    add_column :users, :image, :string,
-    add_column :users, :uid, :string,
-    add_column :users, :oauth_expires_at, :integer,
-    add_column :users, :oauth_token, :string,
+    add_column :users, :provider, :string
+    add_column :users, :image, :string
+    add_column :users, :uid, :string
+    add_column :users, :oauth_expires_at, :integer
+    add_column :users, :oauth_token, :string
     add_column :users, :name, :string
   end
 end
---
+++ db/migrate/20130404222103_set_default_email.rb
@@ -0,0 +1,7 @@
+class SetDefaultEmail < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end
