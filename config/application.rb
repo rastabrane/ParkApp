700bc916385b2163a8efdd0d9917f1e0a86c019f
Buck O'Leary <howardeoleary@gmail.com>
2013-04-19 13:20:21 -0400

    added lobster and easternmarket fonts and added font compiling to asset pipeline

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,13 @@ module ParkApp
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
+    # Add the fonts path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
+    
+    
     config.assets.initialize_on_precompile = false
   end
 end
