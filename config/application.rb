c11934b412a75f8ade54b8db53d08f55054feac7
Buck O'Leary <howardeoleary@gmail.com>
2013-05-07 02:10:06 -0400

    straightening out courts index layout

--- config/application.rb
+++ config/application.rb
@@ -59,8 +59,8 @@ module ParkApp
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
-    # Add the fonts path
-    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    # Add the s path
+    config.assets.paths << "#{Rails.root}/app/assets/s"
     
     # Precompile additional assets
     config.assets.precompile += %w( .svg .eot .woff .ttf )
